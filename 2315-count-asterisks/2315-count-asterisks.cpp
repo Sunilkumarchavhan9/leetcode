19a106ec8d05a2d6e75f5b0babf427cac9b950fd
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-08-29 13:53:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (71.64%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        int count=0;
-        int flag=false;
-        
-        for(char c:s){
-            if(c == '|'){
-                flag =!flag;
-            }else if(c == '*' && !flag){
-                count++;
-            }
-        }
+       int count=0;
+       int flag = false;
+       for(char c:s){
+           if(c == '|'){
+               flag =!flag;
+           }else if(c == '*' && !flag){
+               count++;
+           }
+       }
         return count;
     }
 };
