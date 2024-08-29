5b987b58e0aa41ae568b5a7ffc00ef4e5e07b93e
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-08-29 14:02:57 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (50.84%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-       int i=0 , j=s.size()-1;
+       int  i=0, j=s.size()-1;
         while(i <= j){
             if(!isalpha(s[i])){
                 i++;
             }else if(!isalpha(s[j])){
                 j--;
             }else{
-                swap(s[i] , s[j]);
-                    i++;
-                    j--;
+                swap(s[i], s[j]);
+                i++;
+                j--;
             }
         }
         return s;
