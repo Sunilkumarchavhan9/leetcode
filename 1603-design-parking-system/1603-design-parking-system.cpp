67cfcc699a5e2fa990e10831e6b6c9924f094a6b
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-08-31 12:32:35 +0530

    Time: 46 ms (66.90%), Space: 37.1 MB (34.96%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -42,8 +42,3 @@ public:
     }
 };
 
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem* obj = new ParkingSystem(big, medium, small);
- * bool param_1 = obj->addCar(carType);
- */
