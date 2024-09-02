7f051175756f14d7ae1db71800a3c99b375f54bd
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-02 21:30:49 +0530

    Time: 15 ms (61.05%), Space: 15.7 MB (77.55%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-         int n=mat.size();
-         int m=mat[0].size();
+       int n=mat.size();
+       int m=mat[0].size();
         
-         int row[n];
-         int col[m];
-         
-         for(int i=0; i<n; ++i)
+        int row[n];
+        int col[m];
+        
+        for(int i=0; i<n; ++i){
             row[i]=0;
-         for(int j=0; j<m; j++)
-            col[j]=0; 
-        
-        for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
+                col[j]=0;
+            }
+        }
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<m; ++j){
                 if(mat[i][j] == 1){
                     row[i]++;
                     col[j]++;
                 }
             }
         }
-         int speacial=0;
-          for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                if(mat[i][j] ==1 && row[i] == 1 && col[j] == 1)
+        int speacial=0;
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<m; ++j){
+                if(mat[i][j] && row[i] == 1 && col[j] == 1){
                     speacial++;
                 }
-          }    
-          return speacial;    
+            }
+        }
+        return speacial;
     }
 };
