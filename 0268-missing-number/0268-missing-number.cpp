0014508145bd4db157d3335572f7d31885613f8e
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-05 19:55:19 +0530

    Time: 4 ms (98.60%), Space: 20.5 MB (85.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,7 +5,7 @@ public:
        int expected_sum=n*(n+1)/2;
        int actual_sum=0;
         for(int i=0; i<n; i++ ){
-                actual_sum+=nums[i];   
+            actual_sum+=nums[i];   
         }
         return expected_sum - actual_sum;
     }
