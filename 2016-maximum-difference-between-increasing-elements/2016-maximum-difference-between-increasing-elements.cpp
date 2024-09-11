7d206726e42e14e6243b9f111be0ddb4b6203da3
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-11 18:00:06 +0530

    Time: 5 ms (51.21%), Space: 10.9 MB (45.66%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -4,10 +4,11 @@ public:
         int n=nums.size();
         int ans=-1;
         int min_ele=INT_MAX;
+        
         for(int j=0; j<n; j++){
             if(nums[j] > min_ele)
-                ans=max(ans, nums[j] - min_ele);
-            min_ele = min(min_ele, nums[j]);
+                ans = max(ans,nums[j] - min_ele);
+             min_ele = min (min_ele, nums[j]);
         }
         return ans;
     }
