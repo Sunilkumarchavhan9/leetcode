e1aa4e7cd08b9d33407aab0179e6fa62e0f817e9
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-16 18:44:20 +0530

    Time: 83 ms (25.74%), Space: 70.6 MB (22.57%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int bag=0;
-        int ans=INT_MIN;
-        
-        for(int  a : nums){
-            bag+= a;
-            ans = max(ans , bag);
-            if(bag < 0)  bag =0;
-        }    
-     
-            //if some time bag smaller then 0  means '-'no. make as pos no as =0;
+     int maxSubArray(vector<int>& nums){
+     int n=nums.size();
+         int bag=0;
+         int ans=INT_MIN;
+         
+         for(int i=0; i<n; i++){
+            bag+=nums[i];
+             ans = max(ans,bag);
+             if(bag < 0) bag=0;
+         }
+            //if some time bag smaller then 0  means '-'no. make as pos no as =0;;
         return ans;
     }
 };
