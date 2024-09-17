9d6df4d50c71b2b0bbe0e018c090a0c17f213a16
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-17 18:32:00 +0530

    Time: 93 ms (91.88%), Space: 68 MB (63.33%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -4,27 +4,27 @@ public:
         c=tolower(c);
         return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     }
-    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-      int n=words.size();
-        vector<int>pref(n,0);
-        for(int i=0; i<n; ++i){
-            if(isVowels(words[i][0]) && isVowels(words[i].back())){
-               // first and last c index same  we initialize as a 1;
-                pref[i] = 1;
-            }
-        }
-        for(int i=1; i<n; ++i){
-            pref[i] += pref[i-1];
-        }
-        vector<int>result;
-        for(int i=0; i<queries.size(); ++i){
-            int l=queries[i][0], r=queries[i][1];
-            if(l == 0){
-                result.push_back(pref[r]);
-            }else{
-                result.push_back(pref[r] - pref[l-1]);
-            }
-        }
-        return result;
+      vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries){
+          int n=words.size();
+          vector<int>freq(n,0);
+          
+          for(int i=0; i<n; ++i){
+              if(isVowels(words[i][0]) && isVowels(words[i].back())){
+                           freq[i]=1;
+              }
+          }
+          for(int i=1; i<n; ++i){
+              freq[i]+=freq[i-1];
+          }
+           vector<int>result;
+           for(int i=0; i<queries.size(); ++i){
+               int l=queries[i][0], r=queries[i][1];
+               if(l == 0){
+                   result.push_back(freq[r]);
+               }else{
+                   result.push_back(freq[r]-freq[l-1]);
+               }
+           }
+           return result;                
     }
 };
