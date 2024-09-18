c72c2c10fe95636db2b3f15250e5fe99a378175d
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-18 21:50:49 +0530

    Time: 22 ms (43.56%), Space: 27.7 MB (51.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    void rotate_1(vector<int>&nums,int k){
-        int n=nums.size();
-        vector<int>temp(k);
-        for(int i=n-k; i<n; i++){
-            temp[i-n+k] = nums[i];
-        }
-        for(int i=n-k-1; i>=0; i--){
-            nums[i+k]=nums[i];
-        }
-        for(int i=0; i<k; i++){
-            nums[i]=temp[i];
-        }  
-    }
+   // void rotate_1(vector<int>&nums,int k){
+        //int n=nums.size();
+       // vector<int>temp(k);
+        //for(int i=n-k; i<n; i++){
+           // temp[i-n+k] = nums[i];
+       // }
+       // for(int i=n-k-1; i>=0; i--){
+           // nums[i+k]=nums[i];
+       // }
+       // for(int i=0; i<k; i++){
+            //nums[i]=temp[i];
+        //}  
+    //}
     void rotate(vector<int>& nums, int k) {
-        k=k%(nums.size());
-       rotate_1(nums, k);
-        for(int num : nums){
-            cout<<num<<" ";
-        }
+       //k=k%(nums.size());
+       // rotate_1(nums, k);
+       // for(int num : nums){
+    //cout<<num<<" ";
+     // }      
+        
+        k = k % (nums.size());
+        int n=nums.size();
+        
+        reverse(nums.begin(), nums.begin() + n-k);
+        reverse(nums.begin() + n-k, nums.end());
+        reverse(nums.begin(), nums.end());
     }
 };
