882dbd6c2e49530a878bc9a285d19e32e024520e
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-20 18:39:48 +0530

    Time: 98 ms (43.83%), Space: 74.4 MB (28.68%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -6,10 +6,8 @@ public:
         int temp=0, ans=0;
         
         for(int i=0; i<n; i++){
-            //checking here how many even comes
             if(nums[i]%2==0) temp++;
             else{
-                //if after even one or more odd come i am pushing or stroing my even value make  0
                 even.push_back(temp);
                 temp=0;
             }
@@ -17,9 +15,6 @@ public:
         even.push_back(temp);
         int m=even.size();
         for(int i=0; i<m-k; i++){
-         //sliding window cal culation 11211
-            // 1121
-            // 1211
             ans+=(even[i]+1)*(even[i+k]+1);
         }
         return ans;
