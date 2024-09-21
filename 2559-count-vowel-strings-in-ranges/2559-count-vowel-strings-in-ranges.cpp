255cc08c4cf3c954a2ea05e2e2e1fb117962ed67
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-21 19:43:01 +0530

    Time: 111 ms (47.53%), Space: 68 MB (69.96%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -7,24 +7,23 @@ public:
       vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries){
           int n=words.size();
           vector<int>freq(n,0);
-          
-          for(int i=0; i<n; ++i){
-              if(isVowels(words[i][0]) && isVowels(words[i].back())){
-                           freq[i]=1;
+          for(int i=0; i<n; i++){
+              if(isVowels(words[i][0] ) && isVowels(words[i].back()) ){
+                    freq[i]=1;
               }
           }
-          for(int i=1; i<n; ++i){
+          for(int i=1; i<n; i++){
               freq[i]+=freq[i-1];
           }
-           vector<int>result;
-           for(int i=0; i<queries.size(); ++i){
-               int l=queries[i][0], r=queries[i][1];
-               if(l == 0){
-                   result.push_back(freq[r]);
-               }else{
-                   result.push_back(freq[r]-freq[l-1]);
-               }
-           }
-           return result;                
+          vector<int>result;
+          for(int i=0; i<queries.size(); i++){
+              int l=queries[i][0],r=queries[i][1];
+              if(l == 0){
+                  result.push_back(freq[r]);
+              }else{
+                  result.push_back(freq[r] - freq[l-1]);
+              }
+          }
+          return result;
     }
 };
