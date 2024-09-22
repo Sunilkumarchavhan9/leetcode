e88c4f6d78037e14afe1ed8bd16c756810eea0ca
SUNIL KUMAR <chavhansunilkumar753@gmail.com>
2024-09-22 16:45:33 +0530

    Time: 129 ms (89.27%), Space: 143.4 MB (11.98%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -9,12 +9,12 @@ public:
                 neg.push_back(nums[i]);
             }
         }
-        int ind1=0,ind2=0;
-            while(ind2 < nums.size()/2){
-                ans.push_back(pos[ind1]);
-                ind1++;
-                ans.push_back(neg[ind2]);
-                ind2++;
+        int ind1=0, ind2=0;
+        while(ind2 < nums.size()/2){
+            ans.push_back(pos[ind1]);
+            ind1++;
+            ans.push_back(neg[ind2]);
+            ind2++;
         }
         return ans;
     }
