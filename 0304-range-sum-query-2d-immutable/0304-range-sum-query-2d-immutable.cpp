73edd2b6431c5a472554f211023e8db3a4c0d833
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-09-24 10:52:59 +0530

    Time: 282 ms (88.25%), Space: 148.3 MB (18.61%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -2,17 +2,17 @@ class NumMatrix {
 public:
     int pref[201][201] ={0};
     NumMatrix(vector<vector<int>>& matrix) {
-       int n=matrix.size();
+        int n=matrix.size();
         int m=matrix[0].size();
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                pref[i][j] = (i-1 >=0 ? pref[i-1][j] : 0)+
-                    (j-1 >=0 ? pref[i][j-1]: 0)-
-                    ( (i-1 >= 0 && j-1>= 0)? pref[i-1][j-1]:0)+matrix[i][j];
+                pref[i][j]=(i-1 >= 0 ? pref[i-1][j] : 0) +
+                    (j-1 >= 0 ? pref[i][j-1] : 0)-
+                    ( (i-1 >= 0 && j-1 >= 0) ? pref[i-1][j-1] : 0 )+matrix[i][j];
             }
         }
-    }    
+     }    
     int sumRegion(int t, int l, int b, int r) {    
         return pref[b][r] - 
             (t-1 >=0  ? pref[t-1][r] : 0)-
