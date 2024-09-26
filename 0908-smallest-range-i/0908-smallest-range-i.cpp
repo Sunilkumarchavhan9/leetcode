53790edca2a621dca984c5e0551b6a6403f01cf9
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-09-26 18:49:28 +0530

    Time: 13 ms (39.02%), Space: 17.9 MB (99.78%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.cpp
+++ 0908-smallest-range-i/0908-smallest-range-i.cpp
@@ -1,12 +1,19 @@
+
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
-        int n=nums.size();
-        int maxVal = *max_element(nums.begin(), nums.end());
+       int n=nums.size();
+       int maxVal = *max_element(nums.begin(), nums.end());
         int minVal = *min_element(nums.begin(), nums.end());
         
-        int diff = maxVal - minVal ;
-        
+        int diff= maxVal - minVal;
         return max(0, diff - 2*k);
     }
 };
+
+
+
+
+
+
+ //adjust the difference by subtracting 2*k
