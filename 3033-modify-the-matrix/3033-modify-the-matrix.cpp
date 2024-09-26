9bc5ba0a43d576a4d342cb491e0ff3b18c1344c0
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-09-26 18:19:59 +0530

    Time: 7 ms (85.00%), Space: 22.5 MB (79.10%) - LeetHub

--- 3033-modify-the-matrix/3033-modify-the-matrix.cpp
+++ 3033-modify-the-matrix/3033-modify-the-matrix.cpp
@@ -5,17 +5,17 @@ public:
         int m=matrix[0].size();
         vector<vector<int>>ans=matrix;
         for(int j=0; j<m; j++){
-            int max_val=INT_MIN;
+            int max_ele=INT_MIN;
             for(int i=0; i<n; i++){
                 if(matrix[i][j] != -1){
-                     max_val= max(max_val, matrix[i][j]);  
-                }    
+                    max_ele = max(max_ele, matrix[i][j]);
+                }
             } 
             for(int i=0; i<n; i++){
                 if(matrix[i][j] == -1){
-                    ans[i][j]=max_val;
+                    ans[i][j] = max_ele;
                 }    
-            }
+           }
         }
         return ans;
     }
