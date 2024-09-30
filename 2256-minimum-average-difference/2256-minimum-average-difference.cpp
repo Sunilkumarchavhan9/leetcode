241764bd7a06b1dcf84e30bcec90a3cf222020e2
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-09-30 19:35:53 +0530

    Time: 84 ms (29.83%), Space: 87.2 MB (31.75%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
       int n=nums.size();
-          vector<long>pref(n+1);
-          for(int i=0; i<n; i++){
-              pref[i+1] = pref[i] + nums[i];
-          }
-        
-        int ansIndx=-1;
-        long minAvgdiff = LONG_MAX;
+        vector<long>pref(n+1);
         for(int i=0; i<n; i++){
-            int l= pref[i+1]/(i+1);
+            pref[i+1] = pref[i]+nums[i];
+        }
+        int ansIndex=-1;
+        long max_avg=LONG_MAX;
+        for(int i=0; i<n; i++){
+            int l=pref[i+1]/(i+1);
             int r=0;
-            if(i <n-1) r=(pref[n] - pref[i+1])/(n - i - 1);
-            long curr_def = abs(l-r);
-            if(curr_def < minAvgdiff){
-                 minAvgdiff = curr_def;
-                ansIndx=i;
+            if(i < n-1) r=(pref[n] - pref[i+1])/(n-i-1);
+            long curr_deff = abs(l-r);
+            if(curr_deff < max_avg){
+                max_avg=curr_deff;
+                ansIndex =i;
             }
         }
-        return ansIndx;
+        return ansIndex;
     }
 };
