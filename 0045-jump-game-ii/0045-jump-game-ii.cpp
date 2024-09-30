d1cf77d4d062f8ce5bf8769cda3395bd437f914d
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-09-30 21:43:13 +0530

    Time: 8 ms (80.00%), Space: 19.4 MB (41.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-       if(n == 1 ) return 0;
-        
+        if(n == 1)return 0;
         int jump=0;
-        int minjp=0;
+        int min_jump=0;
         int endjump=0;
-        for(int i=0; i,n-1; i++){
-            minjp = max(minjp, i+nums[i]);
+        for(int i=0; i<n-1; i++){
+            min_jump = max(min_jump, i+nums[i]);
             if(i == endjump){
                 jump++;
-                endjump = minjp;
-                
+                endjump = min_jump;
                 if(endjump >= n-1)break;
             }
         }
