a9a6b0837f0632afbc92f0729eec1027073fd362
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-09-30 21:20:02 +0530

    Time: 19 ms (19.87%), Space: 51.9 MB (48.57%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         sort(prices.begin(), prices.end());
-        if(prices[0]+prices[1] <= money){
-            return money - (prices[0] + prices[1]);
-        }return money;
-        
+        for(int i=0; i<prices.size(); i++){
+            if(prices[0]+prices[1] <= money){
+                return money - (prices[0]+prices[1]);
+            }
+        }
+        return money;
     }
 };
