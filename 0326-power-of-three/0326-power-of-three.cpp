296756664d64822a367b831bb03d9a6f38dc4245
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-10-01 21:28:37 +0530

    Time: 16 ms (37.22%), Space: 8.5 MB (74.26%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfHelper(int n) {
+        if(n == 1) return true;
+        if(n%3 != 0)return false;
+        return  isPowerOfHelper(n/3);
+    }
+    bool isPowerOfThree(int n){
+        if(n <= 0)return false;
+        return isPowerOfHelper(n);
+    }
+};
