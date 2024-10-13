36fcbbb2b0142ede231388a418fa351fe1be2402
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-10-13 15:49:38 +0530

    Time: 1751 ms (5.19%), Space: 536.6 MB (6.89%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    vector<int> mergeSortedArrays(vector<int> &left, vector<int> &right) {
-        int i=0, j=0;
-        vector<int>ans;
-        while(i < left.size() and j < right.size()){
-            if(left[i] <= right[j])ans.push_back(left[i++]);
-            else ans.push_back(right[j++]);
-        }
-        while(i < left.size())ans.push_back(left[i++]);
-        while(j < right.size())ans.push_back(right[j++]);
-        return ans;
-    }
+   vector<int>sort2Array(vector<int>left, vector<int>right){
+       int i = 0, j= 0;
+       vector<int>ans;
+       while(i < left.size() and j < right.size()){
+           if(left[i] <= right[j])ans.push_back(left[i++]);
+           else ans.push_back(right[j++]);
+       }
+       while(i < left.size())ans.push_back(left[i++]);
+       while(j < right.size())ans.push_back(right[j++]);
+       return ans;
+   }
 public:
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size();
@@ -18,7 +18,7 @@ public:
         vector<int>right(nums.begin()+n/2, nums.end());
         left = sortArray(left);
         right = sortArray(right);
-        return mergeSortedArrays(left, right);
+        return sort2Array(left, right);
         
     }
 };
