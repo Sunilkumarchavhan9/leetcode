10027ca28dac03d694f20fbd6d80661778ade534
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-11-08 19:04:04 +0530

    Time: 0 ms (100.00%), Space: 101.3 MB (82.66%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-       int n =nums.size() , sum = 0;
+       int n = nums.size() , sum = 0;
       for(int num : nums){
-        sum+= num;
-      }
-      int target = sum - x;
-      int currSum = 0, mxlen = 0;
-      int startindx = 0;
-      bool found = false;
-      
-      for(int endindx=0; endindx<n; ++endindx){
-        currSum +=  nums[endindx];
-        while(startindx <= endindx && currSum > target){
-          currSum -= nums[startindx];
-          ++startindx;
-        }if(currSum == target){
-          found = true;
-          mxlen = max(mxlen, endindx - startindx+1);
-        }
-      }
-      return found ? n- mxlen : -1;
+       sum += num;
     }
+    int target = sum - x;
+    int currsum = 0, maxlen = 0;
+    int startindx = 0;
+    bool found = false;
+    for(int endindx = 0; endindx < n; ++endindx){
+        currsum += nums[endindx];
+      while(startindx <= endindx && currsum > target){
+          currsum -= nums[startindx];
+          ++startindx;
+      }
+      if(currsum == target){
+            found = true;
+        maxlen = max(maxlen, endindx - startindx+1);
+      }
+    }
+     return found ? n - maxlen : -1;
+  }
 };
