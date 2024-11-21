64959542135b93206bdba772075513d75f62d456
Sunil Kumar Chavhan <chavhansunilkumar753@gmail.com>
2024-11-21 18:29:56 +0530

    Time: 3 ms (17.10%), Space: 8.4 MB (44.49%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     string makeGood(string s) {
-     int n=s.length();
-        int flag=true;
-        while(flag){
-            int j=0;
-             n=s.length();
-            flag=false;
-            for(int i=0; i<n; i++){
-                if(i+1 < n && abs(s[i] - s[i+1]) == 32){
-                    flag=true;
-                    i++;
-                }else{
-                    s[j] = s[i];
-                    j++;
-                }   
-                
-            }
-            s=s.substr(0 , j);
+        stack<char>st;
+        string ans = "";
+        for(int i=0; i<s.size(); i++){
+            if(st.empty()) st.push(s[i]);
+            else if(st.top() - 32 == s[i] || st.top() + 32 == s[i]) st.pop();
+            else st.push(s[i]);
         }
-        return s;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 //To solve the problem without using a stack  we can use a two-pointer technique or simply modify the //string in place while iterating over it The idea is to iterate through the string check for adjacent //bad pairs and remove them until the string becomes  good 
